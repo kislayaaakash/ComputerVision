d4abbf7531e258b7a671b99c127719b6a742b469
KISLAYA AAKASH <kislaya.aakash@publicissapient.com>
2020-11-22 21:07:57 +0530

    open image file using opencv

---
+++ mypuppy.py
@@ -0,0 +1,10 @@
+# This piece of code is to read an image using opencv and render it and close it using ESC button
+import cv2
+img = cv2.imread('C:/Users/kisaakas/Downloads/Computer-Vision-with-Python/Computer-Vision-with-Python/DATA/00-puppy.jpg')
+
+while True:
+    cv2.imshow('puppy',img)
+    if cv2.waitKey(1) & 0xFF == 27: # use 
+            break
+            
+cv2.destroyAllWindows()            
